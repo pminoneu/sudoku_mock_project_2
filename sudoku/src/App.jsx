8dc73351f95c0ea7318751393547c887f5ede52e
Paula Minozzo <furquimaraujominoz.p@northeastern.edu>
2025-11-25 22:29:43 -0800

    Updates

--- sudoku/src/App.jsx
+++ sudoku/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Cell from './components/cell.jsx'
 
 function App() {
   return (
     <div>
       <h1>My Sudoku Game</h1>
       <p>It works!</p>
+      
+      <div className="sudoku-board">
+        <Cell />
+      </div>
     </div>
   )
 }
---
+++ sudoku/src/components/cell.jsx
@@ -0,0 +1,9 @@
+function Cell() {
+  return (
+    <div className="square">
+      5
+    </div>
+  )
+}
+
+export default Cell
